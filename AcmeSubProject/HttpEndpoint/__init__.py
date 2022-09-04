2579e47f371e733b5d153df9a0cc8f98c54c6e95
dagoud <dagoudie@gmx.com>
2022-09-04 23:31:58 +0000

    DG went back and added writing to storage table again

--- AcmeSubProject/HttpEndpoint/__init__.py
+++ AcmeSubProject/HttpEndpoint/__init__.py
@@ -6,20 +6,25 @@ import typing
 import azure.functions as func
 
 
-def main(request: func.HttpRequest, msg: func.Out[typing.List[str]]) -> func.HttpResponse:
-    logging.info('HTTP trigger function received a request.')
-
+def main(request: func.HttpRequest, msg: func.Out[typing.List[str]], message: func.Out[str]) -> func.HttpResponse:
+ 
     req_body = request.get_json()
     subtitle = req_body.get("subtitle")
     languages = req_body.get("languages")
+    rowKey = str(uuid.uuid4())
     
-    dataList = []
+    table = json.dumps({
+        "rowKey": rowKey, "subtitle": subtitle,
+    })
+    
+    queueList = []
 
     for lang in languages:
-        dataList.append(json.dumps({
-            "rowKey": str(uuid.uuid4()), "subtitle": subtitle,"languageCode": lang
+        queueList.append(json.dumps({
+            "rowKey": rowKey, "languageCode": lang
         }))
 
-    msg.set(dataList)
+    message.set(table)
+    msg.set(queueList)
 
-    return func.HttpResponse(f"New Message Queue entries created: {dataList}", status_code=200)
+    return func.HttpResponse(f"New Subtitles added to table: {table}. New Language Message Queue entries created: {queueList}", status_code=200)
