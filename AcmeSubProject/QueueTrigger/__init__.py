4909ebe51169e084e56a5139581d3efeec39eb93
dagoud <dagoudie@gmx.com>
2022-09-05 00:16:27 +0000

    DG PART 4 Step 5 WIP

--- AcmeSubProject/QueueTrigger/__init__.py
+++ AcmeSubProject/QueueTrigger/__init__.py
@@ -3,10 +3,12 @@ import json
 import azure.functions as func
 
 
-def main(msg: func.QueueMessage, messageJSON) -> None:
+def main(msg: func.QueueMessage, messageJSON, message: func.Out[str] ) -> None:
     logging.info('Python queue trigger function processed a queue item: %s',
                  msg.get_body().decode('utf-8'))
     
     message = json.loads(messageJSON)
-    print(f"Table row: {messageJSON}")
+    print(f"Table row subitle: {messageJSON}")
+
+    # message.set(message.subtitle.upper())
     # return func.HttpResponse(f"Table row: {messageJSON}")
